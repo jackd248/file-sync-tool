5d60dcd18433ebb69d7fe1310158a3a259452785
Konrad Michalik <konrad.michalik@xima.de>
2020-12-29 11:35:29 +0100

    [Test] Improved tests

--- tests/helper/dummy.sh
+++ tests/helper/dummy.sh
@@ -3,16 +3,25 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-randomFileSize() {
-  echo $((RANDOM%100000000))
+function randomFileSize {
+  echo $((RANDOM%100000000+100000))
 }
 
-# Creat dummy file
-# https://stackoverflow.com/a/9800465
-head -c $(randomFileSize) /dev/urandom > ../files/$1/dir1/dummy1.file
-head -c $(randomFileSize) /dev/urandom > ../files/$1/dir1/dummy2.file
-head -c $(randomFileSize) /dev/urandom > ../files/$1/dir1/dummy3.file
-head -c $(randomFileSize) /dev/urandom > ../files/$1/dir2/dummy1.file
-head -c $(randomFileSize) /dev/urandom > ../files/$1/dir2/dummy2.file
-head -c $(randomFileSize) /dev/urandom > ../files/$1/dir2/dummy3.file
+function createFile {
+  # https://stackoverflow.com/a/9800465
+  head -c $(randomFileSize) /dev/urandom > ../files/$1/$2/dummy$3.file
+}
 
+function createFiles {
+  n=0
+  while (( $n <= $1 ))
+  do
+    createFile $2 $3 $n
+    n=$(( n+1 ))
+  done
+}
+
+for i in dir1 dir2
+do
+  createFiles 20 $1 $i
+done
